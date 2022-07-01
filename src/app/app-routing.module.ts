3e4dd8f2408943946f1edefe60f17c28d3326c0e
unknown <keerthikarnan430@gmail.com>
2022-07-01 16:09:57 -0400

    lifecyclehooks

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
-import { NgModule } from '@angular/core';
+import { Directive, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DirectivesComponent } from './directives/directives.component';
+import { FieldComponent } from './field/field.component';
+import { LifeCycleHooksComponent } from './life-cycle-hooks/life-cycle-hooks.component';
+import { UserformComponent } from './userform/userform.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',redirectTo:"directive",pathMatch:"full"
+  },
+  {
+    path:"directive",component:DirectivesComponent
+  },
+  {
+    path:"life-cycle-hooks",component:LifeCycleHooksComponent,
+  },
+  {
+    path:"userform",component:UserformComponent,
+  },
+  {
+    path:"field",component:FieldComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LifeCycleHooksComponent } from './life-cycle-hooks/life-cycle-hooks.component';
+import { UserformComponent } from './userform/userform.component';
+import { FieldComponent } from './field/field.component';
+import { DirectivesComponent } from './directives/directives.component';
+import { FormsModule , ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LifeCycleHooksComponent,
+    UserformComponent,
+    FieldComponent,
+    DirectivesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
